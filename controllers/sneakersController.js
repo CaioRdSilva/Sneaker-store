dffbbda68d18ac7c027c7d0f934d827094015ef9
CaioRdSilva <caiodrs.silva@gmail.com>
2023-01-30 23:44:46 -0300

    version 0.14

--- controllers/sneakersController.js
+++ controllers/sneakersController.js
@@ -1,11 +1,14 @@
 import { Sneaker } from "../models/Sneaker.js";
 import { User } from "../models/User.js";
 
-export class sneakersController{
-    static async showSneakers(req, res){
-        res.render("sneakers/home")
-    }
-    static async dashboard(req, res){
-        res.render("sneakers/dashboard")
-    }
+export class sneakersController {
+  static async showSneakers(req, res) {
+    res.render("sneakers/home");
+  }
+  static async dashboard(req, res) {
+    res.render("sneakers/dashboard");
+  }
+  static createSneaker(req, res) {
+    res.render("sneakers/create");
+  }
 }
--- routes/sneakersRoutes.js
+++ routes/sneakersRoutes.js
@@ -6,6 +6,7 @@ import { checkAuth } from "../helpers/auth.js";
 //import { checkSeller } from "../helpers/auth.js";
 
 sneakerRouter.get( "/dashboard", checkAuth, sneakersController.dashboard);
+sneakerRouter.get( "/add", checkAuth, sneakersController.createSneaker);
 sneakerRouter.get("/", sneakersController.showSneakers);
 
 export default sneakerRouter;
